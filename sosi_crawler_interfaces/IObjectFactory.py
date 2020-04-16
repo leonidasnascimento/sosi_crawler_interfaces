2489f54396d827bc5986a5866afda54c893fce16
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-04-16 12:43:33 -0300

    Adjustments

--- sosi_crawler_interfaces/IObjectFactory.py
+++ sosi_crawler_interfaces/IObjectFactory.py
@@ -17,6 +17,11 @@ class IObjectFactory(ABC):
     def GetInstance(self, interface: Generic[T], targetCrawlerAlias: str, ) -> T: raise NotImplementedError
     """
     Create an instance of a generic type accoding to a given interface & crawler alias
+
+    :param interface: Interface type to be returned
+    :param targetCrawlerAlias: Target crawler alias. It'll help the Object Factory to find the concrete class to inject
+    :type interface: Generic[T]
+    :type targetCrawlerAlias: str
     """
     
     pass
