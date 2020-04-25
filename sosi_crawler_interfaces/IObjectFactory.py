028d689b2480edddc5ce33142e64015d28c0d40e
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-04-24 23:09:22 -0300

    Inclusion of some arguments on 'LoadDependencies' method

--- sosi_crawler_interfaces/IObjectFactory.py
+++ sosi_crawler_interfaces/IObjectFactory.py
@@ -1,27 +1,44 @@
 from abc import abstractmethod, ABC
 from typing import Generic, TypeVar
 
-T = TypeVar('T')
+InterfaceType = TypeVar('InterfaceType')
+ConcreteClassType = TypeVar('ConcreteClassType')
 
 class IObjectFactory(ABC):
     """
     Interface for SoSI's Depence Injection Concrete Object. It rules how a object factory should work
     """ 
+
     @abstractmethod
-    def LoadDependencies(self): raise NotImplementedError
+    def AddDependency(self, targetCrawler: str, interface: Generic[InterfaceType], concreteClass: Generic[ConcreteClassType]): raise NotImplementedError
     """
-    Load the dependencies set for SoSI's crawlers
+    Add a generic type accoding to a given interface & crawler alias
+
+    :param interface: Interface type to be returned
+    :param targetCrawler: Target crawler alias. It'll help the Object Factory to find the concrete class to inject
+    :param concreteClass: Concrete class type that implements the interface
+    :type concreteClass: ConcreteClassType
+    :type interface: Generic[InterfaceType]
+    :type targetCrawler: str
     """
 
     @abstractmethod
-    def GetInstance(self, interface: Generic[T], targetCrawlerAlias: str, ) -> T: raise NotImplementedError
+    def LoadDependencies(self, filePath: str): raise NotImplementedError
+    """
+    Load the dependencies that were predefined for SoSI's crawlers
+
+    :param filePath: The pre defined dependencies file path
+    :type filePath: str
+    """
+
+    @abstractmethod
+    def GetInstance(self, targetCrawler: str, interface: Generic[InterfaceType]) -> InterfaceType: raise NotImplementedError
     """
     Create an instance of a generic type accoding to a given interface & crawler alias
 
     :param interface: Interface type to be returned
-    :param targetCrawlerAlias: Target crawler alias. It'll help the Object Factory to find the concrete class to inject
-    :type interface: Generic[T]
-    :type targetCrawlerAlias: str
+    :param targetCrawler: Target crawler alias. It'll help the Object Factory to find the concrete class to inject
+    :type interface: Generic[InterfaceType]
+    :type targetCrawler: str
     """
-    
     pass
