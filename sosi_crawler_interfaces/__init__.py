a178328249ebe9d27424a3f5b0246a2b082c424a
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-08-12 15:13:32 -0300

    Adding 'ICrawlingResult' interface to the import line

--- sosi_crawler_interfaces/__init__.py
+++ sosi_crawler_interfaces/__init__.py
@@ -5,3 +5,4 @@ from sosi_crawler_interfaces.ILogging import ILogging
 from sosi_crawler_interfaces.IApiController import IApiController
 from sosi_crawler_interfaces.ICrawler import ICrawler
 from sosi_crawler_interfaces.IObjectFactory import IObjectFactory
+from sosi_crawler_interfaces.ICrawlingResult import ICrawlingResult
