c84a8c28a8af3f1969538fc979576c014f692227
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-11-10 12:48:50 -0300

    Adjustments on logging pipeline

--- sosi_crawler_interfaces/ILogging.py
+++ sosi_crawler_interfaces/ILogging.py
@@ -6,6 +6,11 @@ class ILogging(ABC):
     Logging interface
     """
     
+    __repository: IDataRepository
+
+    def __init__(self):
+        self.__repository = None
+
     @abstractmethod
     def set_repository(self, repository: IDataRepository): raise NotImplementedError
     """
@@ -16,10 +21,31 @@ class ILogging(ABC):
     """
 
     @abstractmethod
-    def log(self, message: str): raise NotImplementedError
+    def _log_pipeline(self, message: str): raise NotImplementedError
     """
-    Performs log operation
+    Add a commando to the log operation pipeline. It can be called many times along the log pipeline execution
+    IMPORTANT: The method that over writes this one should only beresponsible to expose the log message to the end user
 
     :param messagem: A message to log
     :type message: str
     """
+
+    def log(self, message: str):
+        """
+        Performs the log operation pipeline
+
+        :param messagem: A message to log
+        :type message: str
+        """
+        if self.__repository is not None:
+            if not self.__repository.is_connected():
+                self.__repository.connect()
+
+            persisted: bool = self.__repository.execute_command(message)
+
+            self.__repository.disconnect()
+
+            if persisted:
+                self._log_pipeline("** LOG WAS SENT TO THE REPOSITORY **")
+
+        self._log_pipeline(message)
