f847b45966ee5e35cb2588d1192fa59ee8c780a2
Leônidas do Nascimento <leonidas.nascimento@live.com>
2021-01-24 07:14:11 -0300

    Adjustments

--- sosi_crawler_interfaces/IMessageQueue.py
+++ sosi_crawler_interfaces/IMessageQueue.py
@@ -1,8 +1,8 @@
 from abc import abstractmethod, ABC
 
-class IMessageQueue(ABC):
+class IMessageService(ABC):
     """
-    Interface that rules how a MessageQueue component should behave under SoSI's architecture
+    Interface that rules how a MessageService component should behave under SoSI's architecture
     """
 
     def __init__(self):
@@ -12,22 +12,22 @@ class IMessageQueue(ABC):
         pass
 
     @abstractmethod
-    def publish(self, topic: str, message: dict): raise NotImplementedError
+    def publish(self, topic_queue: str, message: dict): raise NotImplementedError
     """
-    Publish a message to a given topic
+    Publish a message/event to a given topic/queue
 
-    :param topic: Target topic name
+    :param topic: Target topic/queue name
     :param message: Object representing the JSON message to be sent to the topic. NOTE: This will be parsed to JSON.
     :type topic: str
     :type message: dict
     """
 
     @abstractmethod
-    def consume(self, topic: str) -> dict: raise NotImplementedError
+    def consume(self, topic_queue: str) -> dict: raise NotImplementedError
     """
-    Consume a message from a topic queue
+    Consume a message from a topic or queue
 
-    :param topic: Target topic name
+    :param topic: Target topic/queue name
     :type topic: str
     :return: dict
     """
